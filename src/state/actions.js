1fd0dbc23993dbbebe2df2c7210b420dfb2cf12e
AWFJ <awfj837@gmail.com>
2024-06-22 17:25:45 +0300

    refactor book fetching logic

--- src/state/actions.js
+++ src/state/actions.js
@@ -1,51 +1,59 @@
 import { createAsyncThunk, createAction } from '@reduxjs/toolkit';
 import axios from 'axios';
-import { BOOKS_TO_LOAD } from '../constants';
+import { BOOKS_TO_LOAD, CATEGORIES } from '../constants';
+
+function constructApiUrl({ title, category, sorting, startIndex }) {
+    const basePart = import.meta.env.VITE_API_URL;
+    const categoryPart = category === "all" ? "" : `+subject:${category}`;
+    const keyPart = `key=${import.meta.env.VITE_API_KEY}`;
+
+    return `${basePart}volumes?q=${title}${categoryPart}&orderBy=${sorting}&startIndex=${startIndex}&maxResults=${BOOKS_TO_LOAD}&${keyPart}`;
+}
+
+function filterBooks(items, storedBooks, fetchedBooksMap, category) {
+    const uniqueBooks = new Map();
+
+    // Remove duplicates and if loading more books, remove books already stored
+    items.forEach(book => {
+        if (!uniqueBooks.has(book.id) && !storedBooks.has(book.id) && !fetchedBooksMap.has(book.id)) {
+            uniqueBooks.set(book.id, book);
+        }
+    });
+
+    // Remove books that don't match the selected category
+    if (category !== CATEGORIES[0]) {
+        [...uniqueBooks].forEach(([id, book]) => {
+            const categories = (book.volumeInfo.categories || []).join(" ").toLowerCase();
+            if (!categories.includes(category)) {
+                uniqueBooks.delete(id);
+            }
+        });
+    }
+
+    return uniqueBooks;
+}
 
 // Fetch books from the Google Books API
 export const fetchBooks = createAsyncThunk('books/fetchBooks', async (_, { getState, rejectWithValue }) => {
     try {
-        let { books, title, sorting, category, startIndex, cachedBooks } = getState().books;
+        const { books, title, sorting, category, cachedBooks } = getState().books;
+        const storedBooksMap = new Map(books.map(book => [book.id, book]));
+        const fetchedBooksMap = new Map(cachedBooks.map(book => [book.id, book]));
+        const fetchedBooks = [...cachedBooks];
 
-        let basePart = import.meta.env.VITE_API_URL;
-        let titlePart = `volumes?q=${title}`;
-        let sortingPart = `orderBy=${sorting}`;
-        let categoryPart = category === "all" ? "" : `+subject:${category}`;
-        let maxResultsPart = `maxResults=${BOOKS_TO_LOAD}`;
-        let keyPart = `key=${import.meta.env.VITE_API_KEY}`;
-
-        let storedBooks = new Map(books.map(book => [book.id, book]));
+        let { startIndex } = getState().books;
         let remainingBooks = BOOKS_TO_LOAD;
-        let fetchedBooks = [...cachedBooks];
-        let FETCHED = new Map(fetchedBooks.map(book => [book.id, book]));
         let booksToCache = [];
         let fetchedTotalItems = 0;
 
         while (remainingBooks > 0) {
-            let startIndexPart = `startIndex=${startIndex}`;
-            const { data: { items, totalItems } } = await axios.get(`${basePart}${titlePart}${categoryPart}&${sortingPart}&${startIndexPart}&${maxResultsPart}&${keyPart}`);
+            const apiUrl = constructApiUrl({ title, category, sorting, startIndex });
+            const { data: { items, totalItems } } = await axios.get(apiUrl);
+            const uniqueBooks = filterBooks(items, storedBooksMap, fetchedBooksMap, category);
+            const count = items.length;
 
             fetchedTotalItems = totalItems;
             startIndex += BOOKS_TO_LOAD;
-            let count = items.length;
-            let uniqueBooks = new Map();
-
-            // Remove duplicates and if loading more books, remove books already stored
-            items.forEach(book => {
-                if (!(uniqueBooks.has(book.id) || storedBooks.has(book.id) || FETCHED.has(book.id))) {
-                    uniqueBooks.set(book.id, book);
-                }
-            });
-
-            // Remove books that don't match the selected category
-            if (category !== 'all') {
-                [...uniqueBooks].forEach(([id, book]) => {
-                    const categories = (book.volumeInfo.categories || []).join(" ").toLowerCase();
-                    if (!categories.includes(category)) {
-                        uniqueBooks.delete(id);
-                    }
-                });
-            }
 
             if (uniqueBooks.size + cachedBooks.length > remainingBooks) {
                 fetchedBooks.push(...Array.from(uniqueBooks.values()).slice(0, remainingBooks - cachedBooks.length));
